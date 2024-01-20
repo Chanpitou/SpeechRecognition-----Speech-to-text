ebbafcd3a0a7d8786afaef75d77249572218b483
Chanpitou <umchanpitou@gmail.com>
2024-01-19 23:44:22 -0600

    add on and off functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import speech_recognition
 
 recognizer = speech_recognition.Recognizer()
+pause = False
 
 # Run the program indefinitely for speech input, until condition break
 while True:
@@ -13,13 +14,20 @@ while True:
         # using Google Web Speech API to recognize the captured audio/speech
         text = recognizer.recognize_google(audio)
         text = text.lower()
-        # display the text
-        print(f"Recognized: {text}")
         # condition to stop the program
-        if text == "mirror off":
-            break
+        if "mirror off" in text:
+            print("screen off")
+            pause = True
+        elif "mirror on" in text:
+            print("screen on")
+            pause = False
+        # display the text if not paused
+        if not pause:
+            print(f"Recognized: {text}")
     # to catch/display error(s)
     except speech_recognition.UnknownValueError:
-        print("Speech Recognition could not understand audio.")
+        if not pause:
+            print("Can't understand you, speak clearer and louder")
     except speech_recognition.RequestError as e:
-        print(f"Could not request results from Google Web Speech API; {e}")
+        if not pause:
+            print(f"Could not request results from Google Web Speech API; {e}")
